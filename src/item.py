97bf7435417be16225190c9a18565397861c028a
Chrisean Nichols <chriseannichols550@gmail.com>
2022-01-16 15:14:42 -0500

    add compatibility with best buy items

--- src/item.py
+++ src/item.py
@@ -1,24 +1,50 @@
-from lib2to3.pgen2.driver import Driver
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-
-DRIVER_PATH = './geckodriver'
+from time import time
+from bs4 import BeautifulSoup
+import datetime
 
 
 class item:
-    def __init__(self, item, store, url, wordbank):
+    def __init__(self, item, store, url, driver, page_source=''):
         self.item = item
         self.store = store
         self.url = url
-        self.wordbank = wordbank
+        self.driver = driver
+        self.page_source = page_source
 
+    # Makes connection to desired webpage of console
     def connect(self):
-        # Configures headless browser
-        options = Options()
-        options.headless = True
-        driver = webdriver.Firefox(executable_path=DRIVER_PATH, options=options)
+        self.driver.get(self.url)
+        self.page_source = self.driver.page_source
+
+    # Chooses the approptiate function to use to check inventory status
+    def check_inventory(self):
+        if self.page_source == '':
+            print('No page source found.. please make connection first')
+            return
         
-        # Makes connection to webpage and prints title
-        driver.get(self.url)
-        print(driver.title)
-        driver.close()
+        if self.store == 'bestbuy':
+            self.__best_buy()
+        else:
+            print('This store is incompatable')
+
+    def __best_buy(self):
+        soup = BeautifulSoup(self.page_source, 'html.parser')
+        output = soup.find_all('button', class_='add-to-cart-button')[0]
+        status = BeautifulSoup(str(output), 'lxml')
+        if status.text == "Sold Out":
+            self.__print_status(False)
+        else:
+            self.__print_status(True)
+
+    def __print_status(self, status):
+        current_time = datetime.datetime.now()
+        date = str(current_time.month)+'-'+str(current_time.day)+'-'+str(current_time.year)
+        time = str(current_time.hour)+':'+str(current_time.minute)
+
+        if status:
+            print('\033[1;32;40m'+self.item+' is AVAILABLE at '+self.store+
+            ' as of '+time+' on '+date+'\033[0;0m')
+        else:
+            print('\033[1;31;40m'+self.item+' is UNAVAILABLE at '+self.store+
+            ' as of '+time+' on '+date+'\033[0;0m')
+        
--- src/main.py
+++ src/main.py
@@ -1,7 +1,36 @@
 from item import item
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+import json
+import time
+from random import randint
 
-print("Starting item watcher with defualt items..")
+DRIVER_PATH = './geckodriver'
 
-url = "https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149"
-obj = item("PS5", "BestBuy", url, [])
-obj.connect()
+# Configures headless browser
+print('Starting headless browser..')
+options = Options()
+options.headless = True
+service = Service(executable_path = DRIVER_PATH)
+driver = webdriver.Firefox(service=service, options=options)
+print('Browser has successfully opened')
+
+# Loads in json file with desired items
+with open('items.json', 'r') as file:
+    data = json.load(file)
+
+# Creates object list with each item
+items = []
+for i in data['items']:
+    items.append(item(i['item'], i['store'], i['url'], driver))
+
+# Check each items stock iteratively
+while True:
+    for i in items:
+        i.connect()
+        i.check_inventory()
+        time.sleep(randint(1, 6))
+
+        
+
