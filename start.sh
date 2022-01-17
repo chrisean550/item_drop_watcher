db24709c2643076c2dafdece43a2e135949a69df
Chrisean Nichols <chriseannichols550@gmail.com>
2022-01-17 14:34:04 -0500

    modify bash script output

--- start.sh
+++ start.sh
@@ -6,7 +6,13 @@ python3 -m venv venv
 
 . venv/bin/activate
 
+echo "Virtual environment started"
+
+echo "Installing dependencies..."
+
 pip install -r requirements.txt > requirements_install_log.txt
+
+echo "Dependencies installed.. check requirements_install_log.txt for output"
 
 python src/main.py
 
