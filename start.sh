65e269b9197223cf1dc8b9d8c968ef770cb3f7f4
Chrisean Nichols <chriseannichols550@gmail.com>
2022-01-17 15:05:03 -0500

    print out dependencies download

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "Welcome to the item drop watcher!"
 
 echo "Starting virtual environment" 
@@ -10,7 +12,9 @@ echo "Virtual environment started"
 
 echo "Installing dependencies..."
 
-pip install -r requirements.txt > requirements_install_log.txt
+pip install -r requirements.txt 
+
+#> requirements_install_log.txt
 
 echo "Dependencies installed.. check requirements_install_log.txt for output"
 
