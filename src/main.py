67702a5b1d4d5d5af74f8184818d515c35ca6eb7
Chrisean Nichols <chriseannichols550@gmail.com>
2022-01-21 16:55:38 -0500

    program sends pings to mongodb

--- src/main.py
+++ src/main.py
@@ -1,22 +1,25 @@
-from ast import Try
 from item import Item
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
+from mongo import MongoConnection
+from dotenv import load_dotenv
 import json
-#import time
-import platform
 from random import randint
 import sys
+import os
+
+load_dotenv()
+
 
 # Determines what firefox driver to use for system
 if sys.argv[1] == "dev":
-    if platform.architecture()[0] == '64bit':
-        DRIVER_PATH = './geckodriver64'
-    else:
-        DRIVER_PATH = '.geckodriver32'
+    DRIVER_PATH = './geckodriver64'
 else:
     DRIVER_PATH = './geckodriver'
+
+HEADLESS = True
+DATABASE = os.environ.get('MONGO_URI')
 
 active = True
 
@@ -25,7 +28,7 @@ def _main():
     # Configures headless browser
     print('Starting headless browser..')
     options = Options()
-    options.headless = True
+    options.headless = HEADLESS
     service = Service(executable_path = DRIVER_PATH)
     driver = webdriver.Firefox(service=service, options=options)
     print('Browser has successfully opened')
@@ -34,17 +37,21 @@ def _main():
     with open('items.json', 'r') as file:
         data = json.load(file)
 
+    # Connecting to mongodb
+    db = MongoConnection(DATABASE)
+    db.connect()
+    
     # Creates object list with each item
     items = []
     for i in data['items']:
-        items.append(Item(i['item'], i['store'], i['url'], driver))
+        items.append(Item(i['item'], i['store'], i['url'], driver, db))
 
     # Check each items stock iteratively
     while active:
         for i in items:
             i.connect()
             i.check_inventory()
-            #time.sleep(randint(1, 6))
+            
 
 try:
     _main()
--- src/mongo.py
+++ src/mongo.py
@@ -1,4 +1,4 @@
-from sre_constants import SUCCESS
+from unittest import result
 import pymongo
 
 class MongoConnection:
@@ -14,6 +14,42 @@ class MongoConnection:
 
         try:
             self.client.server_info()
-            print("Successfull connection")
+            #print("Successfull connection")
         except Exception:
             print("Failed to connect to server")
+
+    def update_last_avaliablity(self, data):
+
+        # Connect to desired cluster
+        db = self.client.item_drop
+        collection = db.last_avaliablity
+
+        # Construct query
+        query = {'item': data['item'], 'store': data['store']}
+        
+        # Update if item exist and insert if not
+        if self.__item_exist(collection, query):
+            collection.update_one(query, {"$set": data}, upsert=True)
+        else:
+            collection.insert_one(data)
+
+    def update_current_avaliablity(self, data):
+        # Connect to desired cluster
+        db = self.client.item_drop
+        collection = db.current_avaliablity
+
+        # Construct query
+        query = {'item': data['item'], 'store': data['store']}
+        
+        # Update if item exist and insert if not
+        if self.__item_exist(collection, query):
+            collection.update_one(query, {"$set": data}, upsert=True)
+        else:
+            collection.insert_one(data)
+
+    def __item_exist(self, collection, query):
+        results = collection.find_one(query)
+        if results == "None":
+            return False
+        else: 
+            return True
